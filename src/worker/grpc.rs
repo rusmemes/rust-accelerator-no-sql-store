4e036199a3aac9209b8a79554f0fa298e9e74dc1
Aleksei Emelianov <aleksei.emelianov@almatech.dev>
2026-07-22 16:55:45 +0200

    worker grpc layer

--- src/worker/grpc.rs
+++ src/worker/grpc.rs
@@ -1,31 +1,38 @@
+mod input;
+mod manager_connection;
 mod output;
 mod session;
-mod manager_connection;
-mod input;
 
 use crate::common::{CommunicationStreamEither, Config, Me, NodeId};
 use crate::conversions::manager_api::v1::WorkerEvent;
+use crate::conversions::worker_api::v1::worker_api_server::WorkerApi;
+use crate::conversions::worker_api::v1::ClientEvent;
+use crate::conversions::worker_api::v1::WorkerEvent as ClientApiWorkerEvent;
 use crate::worker::domain::WorkerProtocol;
 use crate::worker::grpc::output::output;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::RwLock;
-use tonic::Status;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{Request, Response, Status, Streaming};
 
 const GRPC_CONNECTION_CHANNEL_BUFFER_SIZE: usize = 32;
 
 pub(super) type WorkerIOStream =
     CommunicationStreamEither<Sender<Result<WorkerEvent, Status>>, Sender<WorkerEvent>>;
 
-pub struct ManagerApiService {
+type OpenWorkerConnectionStream = ReceiverStream<Result<ClientApiWorkerEvent, Status>>;
+type OpenClientConnectionStream = ReceiverStream<Result<ClientEvent, Status>>;
+
+pub struct WorkerApiService {
     me: Me,
     manager_sessions: Arc<RwLock<HashMap<NodeId, WorkerIOStream>>>,
     tx: Sender<WorkerProtocol>,
     config: Config,
 }
 
-impl ManagerApiService {
+impl WorkerApiService {
     pub fn new(
         (tx, rx): (Sender<WorkerProtocol>, Receiver<WorkerProtocol>),
         me: Me,
@@ -40,12 +47,28 @@ impl ManagerApiService {
             tx,
             config,
         };
-        tokio::spawn(output(
-            me.clone(),
-            tx_clone,
-            rx,
-            manager_sessions_clone,
-        ));
+        tokio::spawn(output(me.clone(), tx_clone, rx, manager_sessions_clone));
         service
+    }
+}
+
+#[tonic::async_trait]
+impl WorkerApi for WorkerApiService {
+    type OpenClientConnectionStream = OpenClientConnectionStream;
+
+    async fn open_client_connection(
+        &self,
+        request: Request<Streaming<ClientEvent>>,
+    ) -> Result<Response<Self::OpenClientConnectionStream>, Status> {
+        todo!()
+    }
+
+    type OpenWorkerConnectionStream = OpenWorkerConnectionStream;
+
+    async fn open_worker_connection(
+        &self,
+        request: Request<Streaming<ClientApiWorkerEvent>>,
+    ) -> Result<Response<Self::OpenWorkerConnectionStream>, Status> {
+        todo!()
     }
 }
