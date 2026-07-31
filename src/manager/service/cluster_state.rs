a6145db6821d3b40b58a967491403d737f2e092a
Aleksei Emelianov <aleksei.emelianov@almatech.dev>
2026-07-31 21:47:36 +0200

    worker: state sync

--- src/manager/service/cluster_state.rs
+++ src/manager/service/cluster_state.rs
@@ -117,15 +117,10 @@ pub(super) fn handle_remove_old_partition(
     partition_id: PartitionId,
     me: &Me,
 ) {
-    let remove = state
-        .partitions
-        .old_replicas
-        .get_mut(&partition_id)
-        .map(|old_replicas| old_replicas.remove(&replica_id) && old_replicas.is_empty())
-        .unwrap_or(false);
-
-    if remove {
-        state.partitions.old_replicas.remove(&partition_id);
+    if let Some(old_replicas) = state.partitions.old_replicas.get_mut(&partition_id) {
+        if old_replicas.remove(&replica_id) && old_replicas.is_empty() {
+            state.partitions.old_replicas.remove(&partition_id);
+        }
     }
 
     if state.elected_leader_id.as_ref() == Some(&me.id) {
